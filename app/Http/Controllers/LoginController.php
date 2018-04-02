afef444c25f7e3e2d22f4b5d4c567c1e19d03f96
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-02 13:50:02 +0530

    Login Middleware and controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    //Declares the middleware and calls it
     public function __construct()
     {
         $this->middleware('Login');
     }
     
+    //Based on the user input details it validates
     public function login( Request $request) 
     {
         $email = $request->email;
@@ -33,20 +35,65 @@ class LoginController extends Controller
             }
             else
             {
-                echo 'Email doesnot exixt';
+                echo 'Email does not exist';
             }
         }
         elseif ($role == 'Teacher')
         {
-            
+            $user = \DB::table('teacher_details')->where('email',$email)->first();
+            if(isset($user))
+            {
+                if(password_verify($password, $user->password))
+                {
+                    echo 'Access Granted';
+                }
+                else
+                {
+                    echo 'Password is incorrect';
+                }
+            }
+            else
+            {
+                echo 'Email does not exist';
+            }
         }
         elseif ($role == 'Student')
         {
-            
+            $user = \DB::table('student_details')->where('email',$email)->first();
+            if(isset($user))
+            {
+                if(password_verify($password, $user->password))
+                {
+                    echo 'Access Granted';
+                }
+                else
+                {
+                    echo 'Password is incorrect';
+                }
+            }
+            else
+            {
+                echo 'Email does not exist';
+            }
         }
         elseif($role == 'Parent')
         {
-            
+            $user = \DB::table('parent_details')->where('email',$email)->first();
+            if(isset($user))
+            {
+                if(password_verify($password, $user->password))
+                {
+                    echo 'Access Granted';
+                }
+                else
+                {
+                    echo 'Password is incorrect';
+                }
+            }
+            else
+            {
+                echo 'Email does not exist';
+            }
         }
     }
 }
--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -15,6 +15,15 @@ class LoginMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $email = $request->email;
+        $password = $request->password;
+        $role = $request->role;
+        
+        //If any of the field is empty, it redirects back to the same page
+        if($email == null || $password == null || $role == null)
+        {
+            return back()->withInput();
+        }    
         return $next($request);
     }
 }
