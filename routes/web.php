dc8e344c511ff31200131433800d6fc24d07d174
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-02 13:18:13 +0530

    routes declaration

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//A.NAVIGATION TO VIEWS 
+//A1. Page to be displayed when the user enters the application
 Route::get('/', function () {
     return view('welcome');
 });
+
+//A2. Navigating to register page
+Route::get('register', function () {
+    return view('register');
+});
+
+
+//B.NAVIGATION TO MIDDLWARES AND CONTROLLERS
+//B1.Navigating to register middleware and controller, post registering
+Route::post('register','RegisterController@register');
