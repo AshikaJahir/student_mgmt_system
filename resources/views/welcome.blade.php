a89cb63b79f37064e02136515f9b4cda7e59fa03
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-02 14:21:03 +0530

    Titles changed - Welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SMS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,9 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Student Management System                    
                 </div>
-
+                
+                
                 <div class="links">
                     
                    <!--<a href="https://laravel.com/docs">Documentation</a>
