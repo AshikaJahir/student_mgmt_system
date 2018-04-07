f02bec2a3f95b535362640bcd0462ce13d5c5ed7
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-07 12:23:25 +0530

    Middleware for storing transport data

---
+++ app/Http/Middleware/TransportDataMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TransportDataMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $transportmode = $request->transportmode;
+        $fee = $request->fee;
+        $studentid = $request->studentid;
+        
+        if($transportmode == null || $fee == null || $studentid == null)
+        {
+            return back()->withInput();
+        }
+        return $next($request);
+    }
+}
