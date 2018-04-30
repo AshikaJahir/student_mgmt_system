eabed4befe296f2867f7e6343d9b40a0eeeedb4d
Ashika Jahir <18389808+AshikaJahir@users.noreply.github.com>
2018-04-30 18:53:49 +0530

    modified crct routes for resource

--- routes/web.php
+++ routes/web.php
@@ -175,37 +175,37 @@ Route::group(['namespace' => '\Teacher'], function(){
 //After the modification of db
 //For course_details
 Route::resource('coursedetails', 'CourseDetailsController');
-Route::post('create','CourseDetailsController@store')->middleware('CourseDetails');
-Route::put('{id}/edit','CourseDetailsController@update')->middleware('CourseDetails');
-Route::DELETE('{id}/delete','CourseDetailsController@destroy');
+Route::post('coursedetails/create','CourseDetailsController@store')->middleware('CourseDetails');
+Route::put('coursedetails/{id}/edit','CourseDetailsController@update')->middleware('CourseDetails');
+Route::DELETE('coursedetails/{id}/delete','CourseDetailsController@destroy');
 
 //For student_details
 Route::resource('studentdetails', 'StudentDetailsController');
-Route::post('create','StudentDetailsController@store')->middleware('StudentDetails');
-Route::put('{id}/edit','StudentDetailsController@update')->middleware('StudentDetails');
-Route::DELETE('{id}/delete','StudentDetailsController@destroy');
+Route::post('studentdetails/create','StudentDetailsController@store')->middleware('StudentDetails');
+Route::put('studentdetails/{id}/edit','StudentDetailsController@update')->middleware('StudentDetails');
+Route::DELETE('studentdetails/{id}/delete','StudentDetailsController@destroy');
 
 //For student_attendance
 Route::resource('studentattendance', 'StudentAttendanceController');
-Route::post('create','StudentAttendanceController@store')->middleware('StudentAttendance');
-Route::put('{id}/edit','StudentAttendanceController@update')->middleware('StudentAttendance');
-Route::DELETE('{id}/delete','StudentAttendanceController@destroy');
+Route::post('studentattendance/create','StudentAttendanceController@store')->middleware('StudentAttendance');
+Route::put('studentattendance/{id}/edit','StudentAttendanceController@update')->middleware('StudentAttendance');
+Route::DELETE('studentattendance/{id}/delete','StudentAttendanceController@destroy');
 
 //For student_subject_paper_mapping
 Route::resource('studentsubjectpaper', 'StudentSubjectPaperMapping');
-Route::post('create','StudentSubjectPaperMapping@store')->middleware('StudentSubject');
-Route::put('{id}/edit','StudentSubjectPaperMapping@update')->middleware('StudentSubject');
-Route::DELETE('{id}/delete','StudentSubjectPaperMapping@destroy');
+Route::post('studentsubjectpaper/create','StudentSubjectPaperMapping@store')->middleware('StudentSubject');
+Route::put('studentsubjectpaper/{id}/edit','StudentSubjectPaperMapping@update')->middleware('StudentSubject');
+Route::DELETE('studentsubjectpaper/{id}/delete','StudentSubjectPaperMapping@destroy');
 
 //For subject_paper_details
 Route::resource('subjectpaperdetails', 'SubjectPaperDetails');
-Route::post('create','SubjectPaperDetails@store')->middleware('SubjectPaper');
-Route::put('{id}/edit','SubjectPaperDetails@update')->middleware('SubjectPaper');
-Route::DELETE('{id}/delete','SubjectPaperDetails@destroy');
+Route::post('subjectpaperdetails/create','SubjectPaperDetails@store')->middleware('SubjectPaper');
+Route::put('subjectpaperdetails/{id}/edit','SubjectPaperDetails@update')->middleware('SubjectPaper');
+Route::DELETE('subjectpaperdetails/{id}/delete','SubjectPaperDetails@destroy');
 
 //For user
 Route::resource('user', 'UserController');
-Route::post('create','UserController@store')->middleware('User');
-Route::put('{id}/edit','UserController@update')->middleware('User');
-Route::DELETE('{id}/delete','UserController@destroy');
+Route::post('user/create','UserController@store')->middleware('User');
+Route::put('user/{id}/edit','UserController@update')->middleware('User');
+Route::DELETE('user/{id}/delete','UserController@destroy');
 
